d218c74294dfdbcf3aeccef83d3b64c41ba61441
Nicholas M Martin <nmmart04@louisville.edu>
2020-10-10 13:58:04 -0400

    Initial scaffolding code

---
+++ Code2Gether-Discord-Bot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Code2Gether_Discord_Bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

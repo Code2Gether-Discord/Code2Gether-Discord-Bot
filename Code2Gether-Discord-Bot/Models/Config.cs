8bfc326c81e6626d1f80775a5057bcbdc9ae098a
Nicholas M Martin <nmmart04@louisville.edu>
2020-10-11 23:15:45 -0400

    Added json config file to contain token and prefix

---
+++ Code2Gether-Discord-Bot/Models/Config.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+
+namespace Code2Gether_Discord_Bot.Models
+{
+    public interface IConfig
+    {
+        string DiscordToken { get; set; }
+        string Prefix { get; set; }
+    }
+
+    public class Config : IConfig
+    {
+        [JsonProperty("prefix")]
+        public string Prefix { get; set; }
+
+        [JsonProperty("token")]
+        public string DiscordToken { get; set; }
+
+        public Config() { }
+
+        public Config(string prefix, string token)
+        {
+            Prefix = prefix;
+            DiscordToken = token;
+        }
+    }
+}

55e698e3062efad77c6af6a010648636295d9a1e
Christian Khalil <christian.khalil2000@gmail.com>
2020-11-22 22:22:35 +0100

    added project class

---
+++ Code2Gether-Discord-Bot/Models/Project.cs
@@ -0,0 +1,25 @@
+﻿using Discord;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Code2Gether_Discord_Bot.Models
+{
+    class Project
+    {
+        public string Name { get; }
+        public List<IUser> ProjectMembers { get; set; } = new List<IUser>();
+        public bool IsActive
+        {
+            get
+            {
+                return ProjectMembers.Count > 2;
+            }
+        }
+        public Project(string name)
+        {
+            Name = name;
+        }
+
+    }
+}

38f061f6e0cf2dac6522a2c6614c9dbdd396ecf4
Nicholas M Martin <nmmart04@louisville.edu>
2020-11-22 21:56:46 -0500

    Project class

---
+++ Code2Gether-Discord-Bot.Library/Models/Project.cs
@@ -0,0 +1,23 @@
+﻿using Discord;
+using System.Collections.Generic;
+
+namespace Code2Gether_Discord_Bot.Library.Models
+{
+    public class Project
+    {
+        public string Name { get; }
+        public List<IUser> ProjectMembers { get; set; } = new List<IUser>();
+        public bool IsActive
+        {
+            get
+            {
+                return ProjectMembers.Count > 2;
+            }
+        }
+
+        public Project(string name)
+        {
+            Name = name;
+        }
+    }
+}

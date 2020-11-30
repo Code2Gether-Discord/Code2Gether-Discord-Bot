7f75f565daa526cacd0f9fee9ac831833ebe9d88
Ari <DonSagiv@users.noreply.github.com>
2020-11-30 16:10:33 -0500

    Add run-time Entity Framework migration.

--- Code2Gether-Discord-Bot.WebApi/Program.cs
+++ Code2Gether-Discord-Bot.WebApi/Program.cs
@@ -1,5 +1,8 @@
+using Code2Gether_Discord_Bot.WebApi.DbContexts;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -11,9 +14,24 @@ namespace Code2Gether_Discord_Bot.WebApi
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public async static Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+
+            try
+            {
+                var dbContext = services.GetRequiredService<DiscordBotDbContext>();
+                await dbContext.Database.MigrateAsync();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>

134a8df99a11c4fabfad1944b6bc44b1d5c9b1bc
Ari <DonSagiv@users.noreply.github.com>
2020-12-12 08:38:39 -0500

    Re-apply table and column nomenclature to Project and Member.

--- Code2Gether-Discord-Bot.Library/Models/Project.cs
+++ Code2Gether-Discord-Bot.Library/Models/Project.cs
@@ -6,13 +6,17 @@ using System.Linq;
 
 namespace Code2Gether_Discord_Bot.Library.Models
 {
+    [Table("PROJECT")]
     public class Project : IDataModel
     {
         #region Properies
+        [Column("PROJECT_ID")]
         [Key]
         public virtual int ID { get; set; }
+        [Column("PROJECT_NAME")]
         [Required]
         public virtual string Name { get; set; }
+        [Column("AUTHOR_ID")]
         public virtual Member Author { get; set; }
         public virtual List<Member> Members { get; set; } = new List<Member>();
         [NotMapped]

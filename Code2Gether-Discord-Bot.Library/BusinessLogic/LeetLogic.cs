a913a4b4c305888e689e7bfae3934e587d5cf493
Nicholas M Martin <nmmart04@louisville.edu>
2020-12-15 21:34:03 -0500

    Scaffolded new leet module and commands

---
+++ Code2Gether-Discord-Bot.Library/BusinessLogic/LeetLogic.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using Code2Gether_Discord_Bot.Library.Models;
+using Discord;
+using Discord.Commands;
+
+namespace Code2Gether_Discord_Bot.Library.BusinessLogic
+{
+    public class LeetLogic : IBusinessLogic
+    {
+        private ILogger _logger;
+        private ICommandContext _context;
+        private string _args;
+
+        public LeetLogic(ILogger logger, ICommandContext context, string args)
+        {
+            _logger = logger;
+            _context = context;
+            _args = args;
+        }
+
+        public Task<Embed> ExecuteAsync()
+        {
+            _logger.Log(_context);
+
+            // Set from a private method
+            string title = string.Empty;    // Title for leet question
+            string description = string.Empty;  // Prompt for leet question
+
+            var embed = new EmbedBuilder()
+                .WithColor(Color.Purple)
+                .WithTitle($"Leet Question: {title}")
+                .WithDescription(description)
+                .WithAuthor(_context.Message.Author)
+                .Build();
+
+            return Task.FromResult(embed);
+        }
+    }
+}
---
+++ Code2Gether-Discord-Bot/Modules/UserModules/LeetModule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Code2Gether_Discord_Bot.Static;
+using Discord.Commands;
+
+namespace Code2Gether_Discord_Bot.Modules.UserModules
+{
+    public class LeetModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("leet", 
+            RunMode = RunMode.Async)]
+        //[Alias()]
+        [Summary("Reply with a question from leetcode.com with a given title.")]
+        public async Task LeetAsync([Remainder] string title = "") =>
+            await ReplyAsync(embed: await BusinessLogicFactory.GetLeetLogic(GetType(), Context, title).ExecuteAsync());
+
+        [Command("leetans",
+            RunMode = RunMode.Async)]
+        [Alias("leetanswer")]
+        [Summary("Provide an answer for an open leet question.")]
+        public async Task LeetAnsAsync([Remainder] string answer = "") =>
+            await ReplyAsync(embed: await BusinessLogicFactory.GetLeetAnsLogic(GetType(), Context, answer).ExecuteAsync());
+    }
+}

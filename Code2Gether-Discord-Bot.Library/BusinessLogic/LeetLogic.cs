0110e6a52e6c71a94bf9c2080996654ddc470071
ephrem-adugna <ephremadugna1219@gmail.com>
2020-12-18 12:47:55 -0800

    DEFINITELY Fixed Null Error

--- Code2Gether-Discord-Bot.Library/BusinessLogic/LeetLogic.cs
+++ Code2Gether-Discord-Bot.Library/BusinessLogic/LeetLogic.cs
@@ -63,7 +63,7 @@ namespace Code2Gether_Discord_Bot.Library.BusinessLogic
             var result = await response.Content.ReadAsStringAsync();
             var desJson = JsonConvert.DeserializeObject<JsonResponse>(result);
             var tagRemover = new Regex(@"<[^>]*>"); // Using Regex to remove HTML tags in JSON response.
-            if (desJson.Data.Question != null)
+            if (desJson.Data?.Question != null)
             {
                 title = desJson.Data.Question.Title;
                 plainDescription = $"*{desJson.Data.Question.TopicTags[0].Name}* \n{desJson.Data.Question.Content}";

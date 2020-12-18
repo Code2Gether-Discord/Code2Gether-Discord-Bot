d2cf3899396c2d577cda60a672d7065caf9d0ef8
Nicholas M Martin <nmmart04@louisville.edu>
2020-12-18 14:16:18 -0500

    Code cleanup. Organized Leetcode API classes.

--- Code2Gether-Discord-Bot.Library/BusinessLogic/LeetLogic.cs
+++ Code2Gether-Discord-Bot.Library/BusinessLogic/LeetLogic.cs
@@ -1,35 +1,37 @@
-﻿using Code2Gether_Discord_Bot.Library.Models;
-using Discord;
-using Discord.Commands;
-using Newtonsoft.Json;
-using System.Net.Http;
+﻿using System.Net.Http;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using static Code2Gether_Discord_Bot.Library.Models.LeetcodeQuestion;
+using Code2Gether_Discord_Bot.Library.Models;
+using Code2Gether_Discord_Bot.Library.Models.LeetModels;
+using Discord;
+using Discord.Commands;
+using Newtonsoft.Json;
 
 namespace Code2Gether_Discord_Bot.Library.BusinessLogic
 {
     public class LeetLogic : IBusinessLogic
     {
-        private ILogger _logger;
-        private ICommandContext _context;
-        private string _args;
+        private readonly string _args;
+        private readonly ICommandContext _context;
+        private readonly ILogger _logger;
+
         public LeetLogic(ILogger logger, ICommandContext context, string args)
         {
             _logger = logger;
             _context = context;
             _args = args;
         }
+
         public async Task<Embed> ExecuteAsync()
         {
             _logger.Log(_context);
 
 
-            var result = await GetQuestions(_args.Replace("-", "").ToLower());
+            var result = await GetQuestions(_args);
             // Set from a private method
-            string title = result.Item1;    // Title for leet question
-            string description = result.Item2;  // Prompt for leet question
+            var title = result.Item1; // Title for leet question
+            var description = result.Item2; // Prompt for leet question
             var embed = new EmbedBuilder()
                 .WithColor(Color.Purple)
                 .WithTitle($"Leet Question: {title}")
@@ -39,13 +41,18 @@ namespace Code2Gether_Discord_Bot.Library.BusinessLogic
 
             return await Task.FromResult(embed);
         }
+
         private async Task<(string, string)> GetQuestions(string questionTitle)
         {
-            var formattedQues = questionTitle.Replace(" ", "-").ToLower(); //Changes spaces to dashes, to be sent in the url.
-            var values = new JSONRequest(formattedQues);
+            var formattedQues =
+                questionTitle
+                    .Replace("-", "") // Remove existing dashes
+                    .Replace(" ", "-") // Changes spaces to dashes, to be sent in the url.
+                    .ToLower();
+            var values = new JsonRequest(formattedQues);
             var json = JsonConvert.SerializeObject(values);
-            string plainDescription = "You requested an invalid question";
-            string title = "Invalid";
+            var plainDescription = "You requested an invalid question";
+            var title = "Invalid";
             var data = new StringContent(json, Encoding.UTF8, "application/json");
 
             const string url = "https://leetcode.com/graphql";
@@ -53,17 +60,17 @@ namespace Code2Gether_Discord_Bot.Library.BusinessLogic
 
             var response = await client.PostAsync(url, data);
 
-            string result = await response.Content.ReadAsStringAsync();
-            var desJson = JsonConvert.DeserializeObject<JSONResponse>(result);
+            var result = await response.Content.ReadAsStringAsync();
+            var desJson = JsonConvert.DeserializeObject<JsonResponse>(result);
             var tagRemover = new Regex(@"<[^>]*>"); //using Regex to remove HTML tags in JSON response.
             if (desJson.Data != null)
             {
                 title = desJson.Data.Question.Title;
-                plainDescription = $"*{desJson.Data.Question.TopicTags[0].Name}* \n" + desJson.Data.Question.Content;
+                plainDescription = $"*{desJson.Data.Question.TopicTags[0].Name}* \n{desJson.Data.Question.Content}";
                 plainDescription = tagRemover.Replace(plainDescription, "").Replace("&nbsp;", "");
             }
+
             return (title, plainDescription);
         }
-
     }
 }
---
+++ Code2Gether-Discord-Bot.Library/Models/LeetModels/LeetcodeApiClasses.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+
+namespace Code2Gether_Discord_Bot.Library.Models.LeetModels
+{
+    public class JsonRequest
+    {
+        [JsonProperty("operationName")] public string OperationName = "questionData";
+
+        [JsonProperty("query")] public string Query =
+            "query questionData($titleSlug: String!) {\n  question(titleSlug: $titleSlug) {\n    questionId\n    questionFrontendId\n    boundTopicId\n    title\n    titleSlug\n    content\n    translatedTitle\n    translatedContent\n    isPaidOnly\n    difficulty\n    likes\n    dislikes\n    isLiked\n    similarQuestions\n    contributors {\n      username\n      profileUrl\n      avatarUrl\n      __typename\n    }\n    topicTags {\n      name\n      slug\n      translatedName\n      __typename\n    }\n    companyTagStats\n    codeSnippets {\n      lang\n      langSlug\n      code\n      __typename\n    }\n    stats\n    hints\n    solution {\n      id\n      canSeeDetail\n      paidOnly\n      hasVideoSolution\n      __typename\n    }\n    status\n    sampleTestCase\n    metaData\n    judgerAvailable\n    judgeType\n    mysqlSchemas\n    enableRunCode\n    enableTestMode\n    enableDebugger\n    envInfo\n    libraryUrl\n    adminUrl\n    __typename\n  }\n}\n";
+
+        [JsonProperty("variables")] public Variables Variables = new Variables();
+
+        public JsonRequest(string questionTitle)
+        {
+            Variables.TitleSlug = questionTitle;
+        }
+    }
+
+    public class Variables
+    {
+        [JsonProperty("titleSlug")] public string TitleSlug;
+    }
+
+    public class JsonResponse
+    {
+        [JsonProperty("data")] public Data Data { get; set; }
+    }
+
+    public class Data
+    {
+        [JsonProperty("question")] public Question Question { get; set; }
+    }
+
+    public class Question
+    {
+        [JsonProperty("title")] public string Title { get; set; }
+
+        [JsonProperty("titleSlug")] public string TitleSlug { get; set; }
+
+        [JsonProperty("content")] public string Content { get; set; }
+
+        [JsonProperty("topicTags")] public Topics[] TopicTags { get; set; }
+    }
+
+    public class Topics
+    {
+        [JsonProperty("name")] public string Name { get; set; }
+    }
+}

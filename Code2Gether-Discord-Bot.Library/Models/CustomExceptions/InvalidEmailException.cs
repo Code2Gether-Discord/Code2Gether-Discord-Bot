ed293a87b41d9f7e5bc639076498c7e67a658bda
Nicholas M Martin <nmmart04@louisville.edu>
2021-01-10 22:20:40 -0500

    Minor formatting

--- Code2Gether-Discord-Bot.Library/Models/CustomExceptions/InvalidEmailException.cs
+++ Code2Gether-Discord-Bot.Library/Models/CustomExceptions/InvalidEmailException.cs
@@ -4,14 +4,14 @@ namespace Code2Gether_Discord_Bot.Library.Models.CustomExceptions
 {
     public class InvalidEmailException : Exception
     {
-        public InvalidEmailException(string message) : base(message)
+        public InvalidEmailException(string message)
+            : base(message)
         {
-            
         }
 
-        public InvalidEmailException(string message, Exception innerException) : base(message, innerException)
+        public InvalidEmailException(string message, Exception innerException)
+            : base(message, innerException)
         {
-            
         }
     }
 }

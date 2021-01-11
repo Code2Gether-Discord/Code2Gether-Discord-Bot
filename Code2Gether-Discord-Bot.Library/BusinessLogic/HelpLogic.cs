be90f638991db442344ee6e0f18b98007aa05360
Nicholas M Martin <nmmart04@louisville.edu>
2021-01-10 22:52:47 -0500

    Broke help output into multiple fields

--- Code2Gether-Discord-Bot.Library/BusinessLogic/HelpLogic.cs
+++ Code2Gether-Discord-Bot.Library/BusinessLogic/HelpLogic.cs
@@ -3,6 +3,7 @@ using Discord;
 using Discord.Commands;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Text;
 using System.Threading.Tasks;
 using Serilog;
@@ -22,33 +23,53 @@ namespace Code2Gether_Discord_Bot.Library.BusinessLogic
 
         public override Task<Embed> ExecuteAsync()
         {
-            var embed = new EmbedBuilder()
-                .WithColor(Color.Purple)
-                .WithTitle("Help")
-                .WithDescription($"Contains information on how to access this bot's command modules")
-                .AddField("Commands", GetCommandText())
-                .WithAuthor(_context.Message.Author)
-                .Build();
+            try
+            {
+                var commandText = GetCommandText();
 
-            return Task.FromResult(embed);
+                var embedBuilder = new EmbedBuilder()
+                    .WithColor(Color.Purple)
+                    .WithTitle("Help")
+                    .WithDescription("Contains information on how to access this bot's command modules")
+                    .WithAuthor(_context.Message.Author);
+
+                for (var i = 0; i < commandText.Length; i++)
+                {
+                    embedBuilder.AddField($"Command {i + 1} of {commandText.Length}", commandText[i]);
+                }
+
+                var embed = embedBuilder.Build();
+
+                return Task.FromResult(embed);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
 
-        private string GetCommandText()
+        private string[] GetCommandText()
         {
-            var commandTextSb = new StringBuilder();
+            var commandTexts = new List<string>();
+
             foreach (var module in _modules)
             {
+                var moduleStringBuilder = new StringBuilder();
+
                 foreach (var command in module.Commands)
                 {
                     var aliasesSb = new StringBuilder();
                     foreach (var alias in command.Aliases)
                     {
-                        aliasesSb.Append($"{alias};");
+                        aliasesSb.Append(alias);
                     }
-                    commandTextSb.Append($"{_prefix}{command.Name} ({aliasesSb}) - {command.Summary}{Environment.NewLine}");
+                    moduleStringBuilder.Append($"{_prefix}{command.Name} ({aliasesSb}) - {command.Summary}{Environment.NewLine}");
                 }
+
+                commandTexts.Add(moduleStringBuilder.ToString());
             }
-            return commandTextSb.ToString();
+            return commandTexts.ToArray();
         }
     }
 }

7812adaf491d35434aecb41d5cb8cdad1a67f176
Nicholas M Martin <nmmart04@louisville.edu>
2021-01-13 21:39:34 -0500

    Deactivate custom project modules

--- Code2Gether-Discord-Bot/Modules/UserModules/ListProjectsModule.cs
+++ Code2Gether-Discord-Bot/Modules/UserModules/ListProjectsModule.cs
@@ -5,13 +5,14 @@ using Discord.Commands;
 
 namespace Code2Gether_Discord_Bot.Modules.UserModules
 {
-    public class ListProjectsModule : ModuleBase<SocketCommandContext>
+    [Obsolete("Deactivated until further notice")]
+    class ListProjectsModule : ModuleBase<SocketCommandContext>
     {
         [Command("listprojects",
             RunMode = RunMode.Async)]
         [Alias("list")]
         [Summary("Lists all projects that users can join.")]
-        public async Task ListProjectsAsync() =>
+        private async Task ListProjectsAsync() =>
             await ReplyAsync(embed: await BusinessLogicFactory.GetListProjectsLogic(GetType(), Context).ExecuteAsync());
     }
 }

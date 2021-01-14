7dae82958f09ee1bb103c181affd52937d2f0ad4
Nicholas M Martin <nmmart04@louisville.edu>
2021-01-13 21:35:26 -0500

    Implement list team members

--- Code2Gether-Discord-Bot.Library/BusinessLogic/GitHubListTeamMembersLogic.cs
+++ Code2Gether-Discord-Bot.Library/BusinessLogic/GitHubListTeamMembersLogic.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
+using Code2Gether_Discord_Bot.Library.Models;
 using Discord;
 using Discord.Commands;
 using GitHubApiWrapper;
+using Newtonsoft.Json.Linq;
 using Serilog;
 
 namespace Code2Gether_Discord_Bot.Library.BusinessLogic
@@ -18,9 +23,62 @@ namespace Code2Gether_Discord_Bot.Library.BusinessLogic
             _gitHubTeamSlug = gitHubTeamSlug;
         }
 
-        public override Task<Embed> ExecuteAsync()
+        public override async Task<Embed> ExecuteAsync()
         {
-            throw new NotImplementedException();
+            var result = await ParseHttpResponseToEmbedContentAsync(await GetTeamMembersAsync());
+
+            var embed = new EmbedBuilder()
+                .WithColor(Color.Purple)
+                .WithTitle(result.Title)
+                .WithDescription(result.Description)
+                .WithAuthor(_context.Message.Author)
+                .Build();
+
+            return embed;
+        }
+
+        private async Task<HttpResponseMessage> GetTeamMembersAsync()
+        {
+            var response = await _client.Client.GetAsync($"teams/{_gitHubTeamSlug}/members");
+            return response;
+        }
+
+        public async Task<EmbedContent> ParseHttpResponseToEmbedContentAsync(HttpResponseMessage response)
+        {
+            var embedContent = new EmbedContent
+            {
+                Title = "GitHub List Team Members: "
+            };
+
+            if (response.IsSuccessStatusCode)
+            {
+                var descriptionSb = new StringBuilder();
+                var json = await response.Content.ReadAsStringAsync();
+                var teamMembersJToken = JToken.Parse(json);
+                var count = teamMembersJToken.Count();
+
+                descriptionSb.Append($"**Total Members on Team {_gitHubTeamSlug}: {count}**{Environment.NewLine}");
+
+                for (var i = 0; i < count; i++)
+                {
+                    descriptionSb.Append($"{i + 1}: {teamMembersJToken[i]["login"]} ({teamMembersJToken[i]["url"]}){Environment.NewLine}");
+                }
+
+                embedContent.Title += "Successful";
+                embedContent.Description = descriptionSb.ToString();
+            }
+            else
+            {
+                var errMsg =
+                    $"There was an error requesting to list the Team's Members! GitHub's reason was: {response.ReasonPhrase}";
+
+                _logger.Error(errMsg);
+
+                embedContent.Title += "Failed";
+                embedContent.Description = errMsg;
+            }
+
+            return embedContent;
         }
     }
 }
